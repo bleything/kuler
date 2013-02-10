54a4599e303903c6f8b2400edc1a41e863649935
Dominik Menke <dmke@tzi.de>
2013-02-10 16:46:05 +0100

    enforcing ruby style (again, oops)

--- lib/kuler/theme.rb
+++ lib/kuler/theme.rb
@@ -26,23 +26,20 @@ class Kuler
 
     ### create a new Kuler::Theme from a Nokogiri::XML::Element
     def initialize( input )
-      @theme_id    = input.at( "//kuler:themeID"     ).text.to_i
-      @title       = input.at( "//kuler:themeTitle"  ).text
-      @tags        = input.at( "//kuler:themeTags"   ).text.gsub(/\s/, '').split(',')
-      @rating      = input.at( "//kuler:themeRating" ).text.to_i
+      @theme_id    = input.at('//kuler:themeID'    ).text.to_i
+      @title       = input.at('//kuler:themeTitle' ).text
+      @tags        = input.at('//kuler:themeTags'  ).text.gsub(/\s/, '').split(',')
+      @rating      = input.at('//kuler:themeRating').text.to_i
 
-      @author_name = input.at( "//kuler:themeAuthor/kuler:authorLabel" ).text
-      @author_id   = input.at( "//kuler:themeAuthor/kuler:authorID"    ).text.to_i
+      @author_name = input.at('//kuler:themeAuthor/kuler:authorLabel').text
+      @author_id   = input.at('//kuler:themeAuthor/kuler:authorID'   ).text.to_i
 
-      @swatches = input.search( "//kuler:swatch" ).map {|nodes| Kuler::Swatch.new nodes }
+      @swatches = input.search('//kuler:swatch').map {|nodes| Kuler::Swatch.new nodes }
     end
-
 
     ### returns an array of hex values of the swatches in this theme
     def hex_codes
-      self.swatches.map {|s| s.hex_code }
+      @hex_codes ||= self.swatches.map {|s| s.hex_code }
     end
-
   end
-
 end # class Kuler
